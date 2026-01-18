13aeae6951cb69f52909deec56ebb31fadda1fb4
Raphael O. <raphael.ob@protonmail.com>
2026-01-18 19:26:39 +0100

    Remove unused member and add comment

--- src/boros_impl/op/mkdir.h
+++ src/boros_impl/op/mkdir.h
@@ -6,9 +6,9 @@
 #include "op/base.h"
 
 typedef struct {
+    /* Mode is stored in base.scratch */
     Operation base;
     PyObject *path;
-    mode_t mode;
 } MkdirOperation;
 
 PyObject *mkdir_operation_create(PyObject *mod, PyObject *const *args, Py_ssize_t nargsf);
